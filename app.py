2b7088952f26256d729cfecc3e1abb3c63abd681
Hibikeyuri <hibikeyuri@gmail.com>
2022-05-12 22:52:47 +0800

    change default things

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import request
+from flask import render_template
+
+
+app = Flask(__name__, static="public", static_url="")
 
 @app.route("/")
 def index():
+    return render_template("index", name="something")
     return "Hello Flask"
 
 def test():
